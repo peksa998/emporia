9072be1eff3e5971706b7604c4fac43786e22884
peksa998 <dejanp775@gmail.com>
2021-11-11 01:51:00 +0100

    v22
    zavrseno sve oko videa

--- assets/js/video.js
+++ assets/js/video.js
@@ -14,3 +14,16 @@
             // kraj
 
 })()
+
+window.onload=function(){
+    w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
+    var targetWidth = 768;
+    if ( w >= targetWidth) {     
+        document.getElementById('video1').innerHTML = '<source src="assets/img/video/v-hero-mob.m4v" type="video/mp4">';
+        console.log("mob 1");
+    }
+    else {
+        document.getElementById('video2').innerHTML = '<source src="assets/img/video/v-hero-desk.m4v" type="video/mp4">';
+        console.log("desk 1");
+    }  
+};
